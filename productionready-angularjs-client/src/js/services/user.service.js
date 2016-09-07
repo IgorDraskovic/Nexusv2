ab2ba7c6f4221808dcbb595b4c549aa15074e1d0
IgorDraskovic <idraskov@gmail.com>
2016-09-07 17:03:09 -0400

    user persists

--- productionready-angularjs-client/src/js/services/user.service.js
+++ productionready-angularjs-client/src/js/services/user.service.js
@@ -1,11 +1,12 @@
 export default class User {
-  constructor(JWT, AppConstants, $http, $state) {
+  constructor(JWT, AppConstants, $http, $state, $q) {
     'ngInject';
 
     this._JWT = JWT;
     this._AppConstants = AppConstants;
     this._$http = $http;
     this._$state = $state;
+    this._$q = $q;
 
     // Object to store our user properties
     this.current = null;
@@ -37,8 +38,49 @@ export default class User {
   logout() {
     this.current = null;
     this._JWT.destroy();
+    //hard reload of current state to flush all data
     this._$state.go(this._$state.$current, null, { reload: true });
-
   }
 
+  verifyAuth() {
+    //promise
+    let deferred = this._$q.defer();
+
+    //check for JWT token
+    //no token
+    if (!this._JWT.get()) {
+      deferred.resolve(false);
+      //resolve now to stop executing - no token
+      return deferred.promise;
+    }
+    //token & current user
+    if (this.current) {
+      deferred.resolve(true);
+
+
+    //if current user not set - get from server
+    } else {
+      this._$http({
+        url: this._AppConstants.api + '/user',
+        method: 'GET',
+        headers: {
+          Authorization: 'Token ' + this._JWT.get()
+        }
+      }).then(
+        (res) => {
+          this.current = res.data.user;
+          deferred.resolve(true);
+        },
+        //error - invalid token
+        (err) => {
+          //wrong token, destroy
+          this._JWT.destroy();
+          deferred.resolve(false);
+        }
+      );
+    }
+
+    return deferred.promise;
+
+  }
 }
