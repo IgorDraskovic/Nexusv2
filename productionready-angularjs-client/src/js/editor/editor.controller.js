96e947971ee8c2e3fd15bb8c4b933f49ec2d72b4
IgorDraskovic <idraskov@gmail.com>
2016-09-13 16:45:55 -0400

    article controller

--- productionready-angularjs-client/src/js/editor/editor.controller.js
+++ productionready-angularjs-client/src/js/editor/editor.controller.js
@@ -2,8 +2,31 @@ class EditorCtrl {
   constructor() {
     'ngInject';
 
+    //article Object
+
+    this.article = {
+      title: '',
+      description: '',
+      body: '',
+      tagList: []
+    }
 
   }
+
+
+
+addTag() {
+  //if tag is not in array...
+  if (!this.article.tagList.includes(this.tagField)) {
+    this.article.tagList.push(this.tagField);
+    this.tagField = '';
+  }
+}
+
+removeTag() {
+  this.article.tagList = this.article.tagList.filter((slug)) => slug !=tagName);
+  }
+
 }
 
 export default EditorCtrl;
